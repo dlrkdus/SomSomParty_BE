75515ad7dd4040633e41cdf279d84fa04fbf27c0
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-12-18 22:13:34 +0900

    feat: 채팅 컴포넌트 추가

---
+++ src/main/java/com/acc/somsomparty/domain/chatting/controller/ChattingController.java
@@ -0,0 +1,11 @@
+package com.acc.somsomparty.domain.chatting.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/festival")
+public class ChattingController {
+
+
+}
