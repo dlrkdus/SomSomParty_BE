733d6ae66ac94401f18dfdba9bc53498bc2c1349
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-12-22 14:39:23 +0900

    fix: 채팅 메세지 시간 추가

--- src/main/java/com/acc/somsomparty/domain/chatting/dto/MessageDto.java
+++ src/main/java/com/acc/somsomparty/domain/chatting/dto/MessageDto.java
@@ -1,12 +1,14 @@
 package com.acc.somsomparty.domain.chatting.dto;
 
 
+import java.time.LocalDateTime;
 import lombok.Builder;
 
 @Builder
 public record MessageDto(
         Long chatRoomId,
         String userName,
+        LocalDateTime createdAt,
         String content
 ) {
 }
