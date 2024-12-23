e53cbb00b603c7a865e41d8bc90495ed89d32b2d
mk-star <ssunn0812@naver.com>
2024-12-23 15:46:02 +0900

    fix: 머지 충돌 해결

--- src/main/java/com/acc/somsomparty/domain/Festival/entity/Festival.java
+++ src/main/java/com/acc/somsomparty/domain/Festival/entity/Festival.java
@@ -1,6 +1,5 @@
 package com.acc.somsomparty.domain.Festival.entity;
 
-import com.acc.somsomparty.domain.Reservation.entity.Reservation;
 import com.acc.somsomparty.domain.Ticket.entity.Ticket;
 import com.acc.somsomparty.global.common.BaseEntity;
 import jakarta.persistence.*;
@@ -11,6 +10,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "festival", indexes = {
+        @Index(name = "idx_name_lower", columnList = "name_lower"),
+        @Index(name = "idx_description_lower", columnList = "description_lower")
+})
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,16 +22,32 @@ public class Festival extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false, length = 20)
     private String name;
+
     @Column(nullable = false, columnDefinition = "TEXT")
     private String description;
+
+    @Column(name = "name_lower", nullable = false, length = 20)
+    private String nameLower;
+
+    @Column(name = "description_lower", nullable = false, columnDefinition = "TEXT")
+    private String descriptionLower;
+
     @Column(name = "start_date", nullable = false)
     private LocalDate startDate;
+
     @Column(name = "end_date", nullable = false)
     private LocalDate endDate;
-    @OneToMany(mappedBy = "festival", cascade = CascadeType.ALL)
-    private List<Reservation> reservationList = new ArrayList<>();
+
     @OneToMany(mappedBy = "festival", cascade = CascadeType.ALL)
     private List<Ticket> ticketList = new ArrayList<>();
+
+    @PrePersist
+    @PreUpdate
+    private void setLowercaseValues() {
+        this.nameLower = this.name.toLowerCase();
+        this.descriptionLower = this.description.toLowerCase();
+    }
 }
