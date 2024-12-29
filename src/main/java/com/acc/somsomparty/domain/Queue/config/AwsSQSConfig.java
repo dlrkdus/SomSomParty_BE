431648901123fdd3127609bbd382816ad7564f3a
mk-star <ssunn0812@naver.com>
2024-12-29 13:37:10 +0900

    chore: yml 환경변수 적용

--- src/main/java/com/acc/somsomparty/domain/Queue/config/AwsSQSConfig.java
+++ src/main/java/com/acc/somsomparty/domain/Queue/config/AwsSQSConfig.java
@@ -17,11 +17,11 @@ import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 @Slf4j
 @Configuration
 public class AwsSQSConfig {
-    @Value("${aws.queue.access-key}")
+    @Value("${spring.cloud.aws.queue.access-key}")
     private String AWS_ACCESS_KEY;
-    @Value("${aws.queue.secret-key}")
+    @Value("${spring.cloud.aws.queue.secret-key}")
     private String AWS_SECRET_KEY;
-    @Value("${aws.queue.region}")
+    @Value("${spring.cloud.aws.queue.region}")
     private String AWS_REGION;
 
     @Bean
