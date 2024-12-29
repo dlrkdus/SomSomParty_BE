1c4742ec7b1dcfe8cea112e821e368665dc8eebe
JIMIN HWANG <hwang0010@naver.com>
2024-12-29 17:47:39 +0900

    feat: 토큰 상태에 따른 알림 전송
    - FCM 토큰의 활성/비활성 상태(TokenState)를 확인하여 비활성화된 토큰으로는 알림을 전송하지 않도록 수정
    - FestivalTokenProjection에 TokenState 필드 추가

--- src/main/java/com/acc/somsomparty/domain/Notification/repository/FcmTokenRepository.java
+++ src/main/java/com/acc/somsomparty/domain/Notification/repository/FcmTokenRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Repository
 public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
     // 축제 시작 날짜를 기준으로 예약자의 fcm token 가져오기
-    @Query("SELECT ft.fcmToken AS fcmToken, f AS festival " +
+    @Query("SELECT ft.fcmToken AS fcmToken, ft.tokenState AS tokenState, f AS festival " +
             "FROM FcmToken ft " +
             "JOIN Reservation r ON ft.userId = r.user.id " +
             "JOIN Ticket t ON r.ticket.id = t.id " +
