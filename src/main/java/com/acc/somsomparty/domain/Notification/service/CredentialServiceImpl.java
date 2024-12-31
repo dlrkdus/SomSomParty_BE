95dfc2c31a55bb42af631ad732f67d6d289cd100
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-01-01 01:17:27 +0900

    chore:CI/CD

--- src/main/java/com/acc/somsomparty/domain/Notification/service/CredentialServiceImpl.java
+++ src/main/java/com/acc/somsomparty/domain/Notification/service/CredentialServiceImpl.java
@@ -5,25 +5,18 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 
 @Service
 @AllArgsConstructor
 public class CredentialServiceImpl implements CredentialService {
-    private NotiAWSConfig notiAwsConfig;
-
-    public AwsCredentialsProvider getAwsCredentials(String accessKeyID, String secretAccessKey) {
-        AwsBasicCredentials awsBasicCredentials =
-                AwsBasicCredentials.create(accessKeyID, secretAccessKey);
-        return () -> awsBasicCredentials;
-    }
 
     public SnsClient getSnsClient() {
         return SnsClient.builder()
-                .credentialsProvider(getAwsCredentials(notiAwsConfig.getNotiAccessKey(),
-                        notiAwsConfig.getNotiSecretKey())
-                ).region(Region.of(notiAwsConfig.getNotiRegion()))
+                .credentialsProvider(DefaultCredentialsProvider.create()) // IAM 역할 기반 인증
+                .region(Region.AP_NORTHEAST_2) // AWS Region 설정
                 .build();
     }
 }
