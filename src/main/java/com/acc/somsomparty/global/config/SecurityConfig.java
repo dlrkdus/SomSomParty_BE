0125036bea7cfb17089a3cca56d23dada6321fc7
mk-star <ssunn0812@naver.com>
2025-01-03 09:43:17 +0900

    fix: CORS 에러 수정

--- src/main/java/com/acc/somsomparty/global/config/SecurityConfig.java
+++ src/main/java/com/acc/somsomparty/global/config/SecurityConfig.java
@@ -5,11 +5,13 @@ import com.acc.somsomparty.global.filter.JwtExceptionFilter;
 import com.acc.somsomparty.global.util.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsUtils;
 
 @Configuration
 @EnableWebSecurity
@@ -29,6 +31,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/reservations/**", "/notification/*").authenticated()
                         .anyRequest().permitAll()
                 )
