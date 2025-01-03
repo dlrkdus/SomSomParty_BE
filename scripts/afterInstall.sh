28770197cc183c77c556f1a344224a722ffba545
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-01-03 21:11:27 +0900

    chore: 스크립트 수정

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -2,34 +2,25 @@
 
 set -e  # 에러 발생 시 스크립트 종료
 
-echo "Starting the deployment process..."
+echo "AfterInstall: Starting the deployment process..."
 
 # 1. AWS ECR 로그인
 echo "Logging in to Amazon ECR..."
 aws ecr get-login-password --region ap-northeast-2 | sudo docker login --username AWS --password-stdin 084828566517.dkr.ecr.ap-northeast-2.amazonaws.com
 
-# 2. Docker 이미지 빌드
-echo "Building the Docker image..."
-sudo docker build -t somparty:latest /home/ubuntu/somparty/.
-
-# 3. ECR로 이미지 태그 추가 및 푸시
-echo "Tagging and pushing the Docker image to ECR..."
-sudo docker tag somparty:latest 084828566517.dkr.ecr.ap-northeast-2.amazonaws.com/somparty:latest
-sudo docker push 084828566517.dkr.ecr.ap-northeast-2.amazonaws.com/somparty:latest
-
-# 4. 기존 Docker Compose 서비스 중지 및 제거
+# 2. 기존 Docker Compose 서비스 중지 및 제거
 echo "Stopping and removing existing Docker Compose services..."
 sudo docker-compose -f /home/ubuntu/somparty/docker-compose.yml down || true
 
-# 5. Docker Compose 이미지 최신화
+# 3. Docker Compose 이미지 최신화 (ECR에서 Pull)
 echo "Pulling the latest Docker images..."
 sudo docker-compose -f /home/ubuntu/somparty/docker-compose.yml pull
 
-# 6. Docker Compose 서비스 재시작
+# 4. Docker Compose 서비스 재시작
 echo "Starting new Docker Compose services..."
 sudo docker-compose -f /home/ubuntu/somparty/docker-compose.yml up -d
 
-# 7. Spring Boot 애플리케이션 상태 확인
+# 5. Spring Boot 애플리케이션 상태 확인
 echo "Validating the Spring Boot application health..."
 status_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/actuator/health || echo "000")
 if [ "$status_code" -eq 200 ]; then
@@ -38,5 +29,3 @@ else
   echo "Spring Boot application validation failed with status code $status_code."
   exit 1
 fi
-
-echo "Deployment completed successfully!"
