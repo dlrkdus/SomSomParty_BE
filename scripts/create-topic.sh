1cf2e773f1fe216287f0146377b9623b25c4fa8b
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-01-04 00:59:03 +0900

    chore: 스크립트 변경

--- scripts/create-topic.sh
+++ scripts/create-topic.sh
@@ -3,8 +3,17 @@
 KAFKA_BROKER="kafka:9092"
 TOPIC="chat-topic"
 
-kafka-topics.sh --create \
+# Kafka가 응답하는지 확인하기 위한 반복문
+until docker exec kafka kafka-topics.sh --list --bootstrap-server $KAFKA_BROKER; do
+  echo "Waiting for Kafka to be ready..."
+  sleep 5
+done
+
+# Kafka가 준비되었으면, 토픽 생성
+docker exec kafka kafka-topics.sh --create \
     --bootstrap-server $KAFKA_BROKER \
     --replication-factor 1 \
     --partitions 3 \
     --topic $TOPIC
+
+echo "Topic '$TOPIC' created."
