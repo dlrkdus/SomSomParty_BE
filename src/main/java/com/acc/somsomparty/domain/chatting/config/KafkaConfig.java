ee9cf831d1d6f2f33fae88ef17b0c8bd728ca009
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-01-04 17:44:16 +0900

    chore: 카프카 설정 변경

--- src/main/java/com/acc/somsomparty/domain/chatting/config/KafkaConfig.java
+++ src/main/java/com/acc/somsomparty/domain/chatting/config/KafkaConfig.java
@@ -16,7 +16,9 @@ import org.springframework.kafka.core.*;
 @EnableKafka
 public class KafkaConfig {
     // 운영 환경에서 브로커 추가시 주소 추가해주면 됨
-    private final String KAFKA_BROKER = "localhost:9092";
+    private final String KAFKA_BROKER =
+            "b-1.sommsk.0hsy6v.c4.kafka.ap-northeast-2.amazonaws.com:9098,"+
+            "b-2.sommsk.0hsy6v.c4.kafka.ap-northeast-2.amazonaws.com:9098";
 
     @Bean
     public ProducerFactory<String, String> producerFactory() {
@@ -24,6 +26,9 @@ public class KafkaConfig {
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKER);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put("security.protocol", "SASL_SSL");
+        config.put("sasl.mechanism", "AWS_MSK_IAM");
+        config.put("sasl.jaas.config", "software.amazon.msk.auth.iam.IAMLoginModule required;");
         return new DefaultKafkaProducerFactory<>(config);
     }
 
@@ -40,6 +45,9 @@ public class KafkaConfig {
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // 가장 처음부터 메시지 읽기
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put("security.protocol", "SASL_SSL");
+        config.put("sasl.mechanism", "AWS_MSK_IAM");
+        config.put("sasl.jaas.config", "software.amazon.msk.auth.iam.IAMLoginModule required;");
         return new DefaultKafkaConsumerFactory<>(config);
     }
 
