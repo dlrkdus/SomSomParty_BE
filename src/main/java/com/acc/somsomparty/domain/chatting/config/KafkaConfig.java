1d9e49db0ec1bc6a856ea45716dbc91d996031ec
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-01-05 00:48:49 +0900

    fix: 카프카 설정 변경

--- src/main/java/com/acc/somsomparty/domain/chatting/config/KafkaConfig.java
+++ src/main/java/com/acc/somsomparty/domain/chatting/config/KafkaConfig.java
@@ -1,8 +1,13 @@
 package com.acc.somsomparty.domain.chatting.config;
 
+import jakarta.annotation.PostConstruct;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.SaslConfigs;
@@ -61,5 +66,34 @@ public class KafkaConfig {
         factory.setConsumerFactory(consumerFactory());
         factory.setConcurrency(3); // 병렬로 처리할 쓰레드 수 설정
         return factory;
+    }
+
+    @PostConstruct
+    public void createTopic() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKER);
+        config.put(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+        config.put(SaslConfigs.SASL_MECHANISM, "AWS_MSK_IAM");
+        config.put(SaslConfigs.SASL_JAAS_CONFIG,"software.amazon.msk.auth.iam.IAMLoginModule required awsProfileName=\"somparty_EC2_IAM\";");
+        config.put(SaslConfigs.SASL_CLIENT_CALLBACK_HANDLER_CLASS, "software.amazon.msk.auth.iam.IAMClientCallbackHandler");
+
+        try (AdminClient adminClient = AdminClient.create(config)) {
+            String topicName = "chat-topic";
+            int partitions = 3; // 파티션 개수
+            short replicationFactor = 2; // 복제본 개수
+
+            // 토픽 존재 여부 확인
+            boolean topicExists = adminClient.listTopics().names().get().contains(topicName);
+            if (!topicExists) {
+                // 토픽 생성
+                NewTopic newTopic = new NewTopic(topicName, partitions, replicationFactor);
+                adminClient.createTopics(Collections.singletonList(newTopic)).all().get();
+                System.out.println("토픽 생성 성공: " + topicName);
+            } else {
+                System.out.println("토픽이 이미 존재합니다: " + topicName);
+            }
+        } catch (ExecutionException | InterruptedException e) {
+            System.err.println("토픽 생성 실패: " + e.getMessage());
+        }
     }
 }
